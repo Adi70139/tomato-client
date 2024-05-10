d556647583ea1eaa6635744e3c9d58415474c0ce
unknown <vardhan70139@gnail.com>
2024-05-10 15:30:14 +0530

    first

---
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './AppDownload.css'
+import { assets } from '../../assets/assets'
+
+const AppDownload = () => {
+
+  
+    return (
+        <div className='app-download' id='app-download'>
+            <p>For Better Experience Download <br />Tomato App</p>
+            <div className="app-download-platforms">
+                <a href='https://play.google.com/store/games?hl=en_IN&gl=US&pli=1'><img src={assets.play_store} alt=""/></a>
+                <a href='https://www.apple.com/in/app-store/'><img src={assets.app_store} alt="" /></a>
+            </div>
+        </div>
+    )
+}
+
+export default AppDownload
